25a10896c4b6c3881575d04ddc69da784dbd5095
ROHIT CHAUHAN <rc672800@gmail.com>
2022-10-31 12:19:39 +0530

    component based call api

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,10 +1,12 @@
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "./../../store/cart-slice";
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
+
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
